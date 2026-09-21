882b2c7674131ecb3dd8e68413373fad9cd248ba
snt-Mikhail <137252470+snt-Mikhail@users.noreply.github.com>
2026-09-21 23:18:26 +0700

    Update kodi-v1.sh

--- ct/kodi-v1.sh
+++ ct/kodi-v1.sh
@@ -251,7 +251,7 @@ export PCT_OPTIONS="
   -unprivileged $CT_TYPE
   $PW
 "
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/ct/create_lxc.sh)" || exit
+#bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/ct/create_lxc.sh)" || exit
 
 msg_info "Pre-starting LXC Container"
 pct start $CTID
